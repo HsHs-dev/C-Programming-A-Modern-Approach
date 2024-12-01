4bef521589993d5600b58021ad14c86739a739bc
HsHs-dev <hassanswritings@outlook.com>
2024-12-01 14:50:03 +0200

    - modifying ex2.c alignment.

--- ch2/Exercises/ex2.c
+++ ch2/Exercises/ex2.c
@@ -1,16 +1,16 @@
-/**********************************************************************
-*  Consider the following program:                                    *
-*  #include <stdio.h>                                                 *
-*  int main(void)                                                     *
-*  {                                                                  *
-*  printf("Parkinson's Law:\nWork expands so as to ");                *
-*  printf("fill the time\n");                                         *
-*  printf("available for its completion.\n");                         *
-*  return 0;                                                          *
-*  }                                                                  *
-*  (a) Identify the directives and statements in this program.        *
-*  (b) What output does the program produce?                          *
-***********************************************************************/
+/*************************************************************************
+*  2. Consider the following program:                                    *
+*  #include <stdio.h>                                                    *
+*  int main(void)                                                        *
+*  {                                                                     *
+*  printf("Parkinson's Law:\nWork expands so as to ");                   *
+*  printf("fill the time\n");                                            *
+*  printf("available for its completion.\n");                            *
+*  return 0;                                                             *
+*  }                                                                     *
+*  (a) Identify the directives and statements in this program.           *
+*  (b) What output does the program produce?                             *
+**************************************************************************/
 
 #include <stdio.h>
 int main(void)
