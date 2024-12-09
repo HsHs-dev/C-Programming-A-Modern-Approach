9c209def4424b6ed3c424b659881732f7d138516
HsHs-dev <hshs@ChokiChoki>
2024-12-09 19:29:57 +0200

    - Adding chapter 4 exercises 5, 6, 7.

---
+++ ch4/Exercises/ex5.c
@@ -0,0 +1,29 @@
+/*
+5. What is the value of each of the following expressions in C89? (Give all possible values if
+an expression may have more than one value.)
+(a) 8 % 5
+(b) -8 % 5
+(c) 8 % -5
+(d) -8 % -5
+*/
+
+#include <stdio.h>
+
+int main(void) {
+
+    
+    printf("8 %% 5 = %d\n", 8 % 5);
+    printf("-8 %% 5 = %d\n", -8 % 5);
+    printf("8 %% -5 = %d\n", 8 % -5);
+    printf("-8 %% -5 = %d\n", -8 % -5);
+
+
+    return 0;
+}
+
+/*
+8 % 5 = 3
+-8 % 5 = -3
+8 % -5 = 3
+-8 % -5 = -3
+*/
---
+++ ch4/Exercises/ex6.c
@@ -0,0 +1,24 @@
+/*
+6. Repeat Exercise 5 for C99.
+*/
+
+#include <stdio.h>
+
+int main(void) {
+
+    
+    printf("8 %% 5 = %d\n", 8 % 5);
+    printf("-8 %% 5 = %d\n", -8 % 5);
+    printf("8 %% -5 = %d\n", 8 % -5);
+    printf("-8 %% -5 = %d\n", -8 % -5);
+
+
+    return 0;
+}
+
+/*
+8 % 5 = 3
+-8 % 5 = -3
+8 % -5 = 3
+-8 % -5 = -3
+*/
