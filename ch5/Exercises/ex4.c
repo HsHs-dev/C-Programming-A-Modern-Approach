ad05b86cc01f485c5f5b18d734d3a2b186b35fe1
HsHs-dev <hassanswritings@outlook.com>
2024-12-22 18:47:38 +0200

    - Adding chapter5 exercises 1 through 4.

---
+++ ch5/Exercises/ex4.c
@@ -0,0 +1,9 @@
+/*
+4. Write a single expression whose value is either –1, 0, or +1, depending on whether i is less
+than, equal to, or greater than j, respectively.
+*/
+
+/*
+From book's website:
+(i > j) - (i < j)
+*/
